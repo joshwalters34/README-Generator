6d66c22b8208317d41f5ccd474bca42b1fd31ec7
JWalters <joshwalters34@gmail.com>
2021-03-21 21:24:27 -0400

    added users questions and started on data to be added to README

---
+++ Develop/README.md
@@ -0,0 +1,8 @@
+
+    <h1>README Generator<h1>
+
+    <h3>Description<h3>
+    A way to create README
+
+    <h3>Table of Contents<h3>
+    
--- Develop/index.js
+++ Develop/index.js
@@ -1,8 +1,63 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = [];
+// const questions = [];
 
+// TODO: Create a function to write README file
+// function writeToFile(fileName, data) {}
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      message: 'What is the title of this README?',
+      name: 'title',
+    },
+    {
+      type: 'input',
+      message: 'Describe your project.',
+      name: 'description',
+    },
+    // {
+    //   type: 'input',
+    //   message: 'What are the installation instructions for this site',
+    //   name: 'instructions',
+    // },
+    // {
+    //     type: 'input',
+    //     message: 'What is the usage information for this site?',
+    //     name: 'usage'
+    // },
+    // {
+    //     type: 'input',
+    //     message: 'Who are the contributors to this site?',
+    //     name: 'contributors'
+    // },
+    // {
+    //     type: 'input',
+    //     message: 'What instructions are needed to test this site?',
+    //     name: 'test'
+    // },
+    // {
+    //     type: 'list',
+    //     message: 'What type of license will be used for this stie?',
+    //     name: 'licence',
+    //     choices: ['MIT', 'Mozilla', 'Apache', 'Eclipse']
+    // }
+  ])
+  .then ((response) => {
+    fs.writeFile('README.md', `
+    ${response.title}
+
+    Description
+    ${response.description}
+
+    Table of Contents
+    `, (err) => 
+    err ? console.log(err) : console.log("README generated")
+    )
+  })
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
