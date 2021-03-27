f03f917f5ac0e3e5138f2ef56ef36ad78b9442c3
JWalters <joshwalters34@gmail.com>
2021-03-27 11:56:13 -0400

    fixed issue with the validation of my prompts and moved inquirer code into init function

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-// TODO: Include packages needed for this application
+
 const inquirer = require('inquirer');
 const fs = require('fs');
-// TODO: Create an array of questions for user input
-// const questions = [];
 
 
 const createReadme = response => ` 
@@ -38,94 +36,82 @@ const createReadme = response => `
   In order to use this generator, users need to ${response.test}
 
 ## Questions
-If you have any questions, please contact me at <${response.email}>.  You can see more of my work at [${response.git}](https://github.com/${response.git}).
+If you have any questions, please contact me at <${response.email}>.  You can see more of my work at [${response.git}](https://github.com/${response.git}){target="_blank"}.
     `
 
- inquirer
+function init() {
+  inquirer
   .prompt([
     {
       type: 'input',
       message: 'What is the title of this README?',
       name: 'title',
-      // validate: validatePrompts,
-      // (input) {
-      //   if (input.length === 0) {
-      //     return "Please enter a title for this README";
-      //   } else {
-      //     return
-      //   }
-      // }
+      validate: validatePrompts,
     },
     {
       type: 'input',
       message: 'Describe your project.',
       name: 'description',
+      validate: validatePrompts,
     },
     {
       type: 'input',
       message: 'What are the installation instructions for this project?',
       name: 'instructions',
+      validate: validatePrompts,
     },
     {
         type: 'input',
         message: 'What is the usage information for this project?',
-        name: 'usage'
+        name: 'usage',
+        validate: validatePrompts,
     },
     {
         type: 'input',
         message: 'Who are the contributors to this projects?',
-        name: 'contributors'
+        name: 'contributors',
+        validate: validatePrompts,
     },
     {
         type: 'input',
         message: 'What instructions are needed to test this project?',
-        name: 'test'
+        name: 'test',
+        validate: validatePrompts,
     },
     {
         type: 'list',
         message: 'What type of license will be used for this project?',
         name: 'license',
-        choices: ['MIT', 'Mozilla', 'Apache', 'Eclipse']
+        choices: ['MIT', 'Mozilla', 'Apache', 'Eclipse'],
+        validate: validatePrompts,
     },
     {
       type: 'input',
       message: 'What is your email address?',
-      name: 'email'
+      name: 'email',
+      validate: validatePrompts,
   },
   {
     type: 'input',
     message: 'What is your GitHub id?',
-    name: 'git'
+    name: 'git',
+    validate: validatePrompts,
 },
   ])
   .then (response => {
     fs.writeFile('README.md', createReadme(response)
     , (err) =>
-    //  => {
-    //   if(response === null) {
-    //     console.log("Please provide an input to each prompt")
-    //   } else console.log("README generated")
-    // }
     err ? console.log(err) : console.log("README generated")
     )
   })
-
-// // TODO: Create a function to write README file
-// function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {
-  // inquirer();
 }
 
-// function validatePrompts(input) {
-//   setTimeout(function() {
-//   if (input.length === 0) {
-//     done("Please enter a title for this README");
-//     return;
-//   } 
-//   done(true);
-// }, 3000)
-// }
+function validatePrompts(input) {
+    if (input.length === 0) {
+      return "Please enter a value for this prompt";
+    } else {
+      return true
+    }
+  }                                   
 // // Function call to initialize app
 init();
